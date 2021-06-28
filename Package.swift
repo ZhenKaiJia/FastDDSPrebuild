f2722988be528ceef05f1904a2825cc22949a215
jianlibao <jianlibao@192.168.0.11>
2021-06-28 17:38:24 +0800

    Build 2.1.0

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "FastDDS",
-                      url: "https://github.com/DimaRU/FastDDSPrebuild/releases/download/2.1.0/fastrtps-2.1.0.xcframework.zip",
-                      checksum: "efdb24d6a6bc3f666a815061d18f836c10c717db01cfbe040d4b8b80b52a3a1b")
+                      url: "https://github.com/ZhenKaiJia/FastDDSPrebuild/releases/download/2.1.0/fastrtps-2.1.0.xcframework.zip",
+                      checksum: "")
     ]
 )
